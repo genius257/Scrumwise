5b9c8f5a64abb60e5f683bf18520f6f82562c547
Anders Pedersen <hot415@hotmail.com>
2022-12-05 02:55:10 +0100

    Added support for getData with parameters for projectIDs and includeProperties

--- src/Methods/Data.ts
+++ src/Methods/Data.ts
@@ -19,10 +19,53 @@ import { Sprint } from "../Objects/Sprint";
 import { Task } from "../Objects/Task";
 import { TimeEntry } from "../Objects/TimeEntry";
 
+export type ProjectIDs = Opaque<string, "ProjectIDs">;
+
+const projectIDsExpression = /^\d+-\d+-\d+(,\d+-\d+-\d+)*$/; //FIXME: validate regex
+
+export function isProjectIDs(unsafe: string): unsafe is ProjectIDs {
+  return projectIDsExpression.test(unsafe);
+}
+
+export function assertProjectIDs(unsafe: string): asserts unsafe is ProjectIDs {
+  if (!isProjectIDs(unsafe)) {
+    throw new Error(`value: "${unsafe}" is not a valid list of project id's`);
+  }
+}
+
+export function projectIDs(unsafe: string): ProjectIDs {
+  assertProjectIDs(unsafe);
+  return unsafe;
+}
+
 export default class Data {
-  public static getData(): Promise<Result<DataObject> | Error> {
+  public static getData(): Promise<Result<DataObject> | Error>; //FIXME: add support for array of strings for projectIDs also
+  public static getData(
+    projectIDs: ProjectIDs
+  ): Promise<Result<DataObject> | Error>;
+  public static getData(
+    projectIDs: ProjectIDs | null,
+    includeProperties: IncludeProperties
+  ): Promise<Result<DataObject> | Error>;
+  public static getData(
+    projectIDs: ProjectIDs | null = null,
+    includeProperties: IncludeProperties | null = null
+  ): Promise<Result<DataObject> | Error> {
     const url = new URL("https://api.scrumwise.com/service/api/v1/getData");
-    url.searchParams.append("includeProperties");
+    if (projectIDs !== null) {
+      url.searchParams.append("projectIDs", projectIDs);
+    }
+    if (includeProperties !== null) {
+      const uniqueIncludeProperties = new Set(includeProperties);
+      const uniqueIncludePropertiesString = Array.from(
+        uniqueIncludeProperties
+      ).join(",");
+
+      url.searchParams.append(
+        "includeProperties",
+        uniqueIncludePropertiesString
+      );
+    }
     return fetch(url).then((response) => response.json());
   }
 }
