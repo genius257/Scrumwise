a65d3654c2f054681b607d7d43aadad34c961b36
Anders Pedersen <hot415@hotmail.com>
2022-12-05 02:33:37 +0100

    Added initial support for endpoint IncludeProperties type

--- src/Methods/Data.ts
+++ src/Methods/Data.ts
@@ -9,6 +9,15 @@ import { Board } from "../Objects/Board";
 import { BoardColumn } from "../Objects/BoardColumn";
 import { ChecklistItem } from "../Objects/ChecklistItem";
 import { Opaque } from "../utils";
+import { File } from "../Objects/File";
+import { Message } from "../Objects/Message";
+import { Project } from "../Objects/Project";
+import { Release } from "../Objects/Release";
+import { Retrospective } from "../Objects/Retrospective";
+import { RetrospectiveCard } from "../Objects/RetrospectiveCard";
+import { Sprint } from "../Objects/Sprint";
+import { Task } from "../Objects/Task";
+import { TimeEntry } from "../Objects/TimeEntry";
 
 export default class Data {
   public static getData(): Promise<Result<DataObject> | Error> {
@@ -17,6 +26,119 @@ export default class Data {
     return fetch(url).then((response) => response.json());
   }
 }
+
+type IncludeProperties = Array<IncludeProperty>; //FIXME: would be nice if it could be indicated as unique, without doing too much ts check computation. Alternative is to make it an object where the keys are the string values.
+type IncludeProperty =
+  | OptionalBacklogItemPropertyValues
+  | OptionalDataPropertyValues
+  | OptionalFilePropertyValues
+  | OptionalMessagePropertyValues
+  | OptionalProjectPropertyValues
+  | OptionalReleasePropertyValues
+  | OptionalRetrospectivePropertyValues
+  | OptionalRetrospectiveCardPropertyValues
+  | OptionalSprintPropertyValues
+  | OptionalTaskPropertyValues
+  | OptionalTimeEntryPropertyValues;
+
+type OptionalBacklogItemPropertyValues =
+  `${BacklogItem["objectType"]}.${OptionalBacklogItemProperties}`;
+type OptionalBacklogItemProperties = Extract<
+  keyof BacklogItem,
+  | "customFieldValues"
+  | "checklists"
+  | "comments"
+  | "attachments"
+  | "timeEntries"
+  | "commits"
+  | "tasks"
+>; //FIXME: define the stings in a way that will trigger a ts error, if a property is later removed (string extends keyof X)
+
+type OptionalDataPropertyValues =
+  `${DataObject["objectType"]}.${OptionalDataProperties}`;
+type OptionalDataProperties = Extract<
+  keyof DataObject,
+  "persons" | "deletedPersons"
+>;
+
+type OptionalFilePropertyValues =
+  `${File["objectType"]}.${OptionalFileProperties}`;
+type OptionalFileProperties = Extract<keyof File, "comments">;
+
+type OptionalMessagePropertyValues =
+  `${Message["objectType"]}.${OptionalMessageProperties}`;
+type OptionalMessageProperties = Extract<
+  keyof Message,
+  "checklists" | "comments" | "attachments"
+>;
+
+type OptionalProjectPropertyValues =
+  `${Project["objectType"]}.${OptionalProjectProperties}`;
+type OptionalProjectProperties = Extract<
+  keyof Project,
+  | "checklists"
+  | "comments"
+  | "attachments"
+  | "customFields"
+  | "tags"
+  | "teams"
+  | "messages"
+  | "backlogs"
+  | "backlogItems"
+  | "releases"
+  | "sprints"
+  | "boards"
+  | "retrospectives"
+  | "files"
+  | "relationships"
+>;
+
+type OptionalReleasePropertyValues =
+  `${Release["objectType"]}.${OptionalReleaseProperties}`;
+type OptionalReleaseProperties = Extract<
+  keyof Release,
+  "customFieldValues" | "checklists" | "comments" | "attachments"
+>;
+
+type OptionalRetrospectivePropertyValues =
+  `${Retrospective["objectType"]}.${OptionalRetrospectiveProperties}`;
+type OptionalRetrospectiveProperties = Extract<
+  keyof Retrospective,
+  "checklists" | "comments" | "attachments"
+>;
+
+type OptionalRetrospectiveCardPropertyValues =
+  `${RetrospectiveCard["objectType"]}.${OptionalRetrospectiveCardProperties}`;
+type OptionalRetrospectiveCardProperties = Extract<
+  keyof RetrospectiveCard,
+  "checklists" | "comments" | "attachments"
+>;
+
+type OptionalSprintPropertyValues =
+  `${Sprint["objectType"]}.${OptionalSprintProperties}`;
+type OptionalSprintProperties = Extract<
+  keyof Sprint,
+  "customFieldValues" | "checklists" | "comments" | "attachments"
+>;
+
+type OptionalTaskPropertyValues =
+  `${Task["objectType"]}.${OptionalTaskProperties}`;
+type OptionalTaskProperties = Extract<
+  keyof Task,
+  | "customFieldValues"
+  | "checklists"
+  | "comments"
+  | "attachments"
+  | "timeEntries"
+  | "commits"
+>;
+
+type OptionalTimeEntryPropertyValues =
+  `${TimeEntry["objectType"]}.${OptionalTimeEntryProperties}`;
+type OptionalTimeEntryProperties = Extract<
+  keyof TimeEntry,
+  "customFieldValues"
+>;
 
 //FIXME: extract optional properties from Objects
 
